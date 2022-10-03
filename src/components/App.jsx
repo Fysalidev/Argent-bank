a6ed2a184b5f4bf50a9ae5c378df4dab6d9df0f3
ROBILLIARD <82404725+Fysalidev@users.noreply.github.com>
2022-10-03 09:49:49 +0200

    Lundi matin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from '../pages/Home'
+import SignIn from '../pages/SignIn'
+import Dashboard from '../pages/Dashboard'
+import Error404 from '../pages/Error404'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Argent Bank</h1>
-    </div>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/user/:id" element={<Dashboard />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    <Footer/>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/argentBankLogo.png";
+
+function Header() {
+  return (
+    <HeaderWrapper>
+      <Logo src={logo} />
+      <Navbar>
+        <NavLink to="/signin">Sign in</NavLink>
+      </Navbar>
+    </HeaderWrapper>
+  );
+}
+
+export default Header;
+
+const HeaderWrapper = styled.header`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  padding: 5px 20px;
+`;
+
+const Logo = styled.img`
+  max-width: 100%;
+  width: 200px;
+`;
+
+const Navbar = styled.nav`
+  font-weight: bold;
+  & a {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    text-decoration: none;
+  }
+  &:hover {
+    text-decoration: underline;
+  }
+`;
