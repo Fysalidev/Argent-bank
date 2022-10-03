eba16fce7857d22015cf768cf2c4c0d7917337be
ROBILLIARD <82404725+Fysalidev@users.noreply.github.com>
2022-10-03 18:49:19 +0200

    Implémentation ok | Formulaire login en cours

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,15 +1,83 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function SignIn() {
+  const [email, setMail] = useState("");
+  const [password, setPassword] = useState("");
+  const [checked, setChecked] = useState(false);
+  const [payload, setPayload] = useState({});
+
+  console.log(email);
+  console.log(password);
+  console.log(checked);
+
+  const handleChangeMail = (event) => {
+    const name = event.target.value;
+    setMail(name);
+  };
+
+  const handleChangePassword = (event) => {
+    const password = event.target.value;
+    setPassword(password);
+  };
+
+  const handleChangeCheckbox = () => {
+    setChecked(!checked);
+  };
+
+  const handleSubmit = () => {
+    const payload = { email: email, password: password };
+    setPayload(payload);
+  };
+
+  useEffect(() => {
+    console.log("Mise à jour du payload");
+    console.log(payload);
+  }, [payload]);
+
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
+        <form>
+          <div className="input-wrapper">
+            <label>Username</label>
+            <input
+              type="email"
+              name="name"
+              onChange={(event) => handleChangeMail(event)}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label>Password</label>
+            <input
+              type="password"
+              name="name"
+              onChange={(event) => handleChangePassword(event)}
+            />
+          </div>
+          <div className="input-remember">
+            <input
+              type="checkbox"
+              checked={checked}
+              onChange={handleChangeCheckbox}
+            />
+            <label>Remember me</label>
+          </div>
+          <input
+            className="sign-in-button"
+            type="submit"
+            value="Sign In"
+            onClick={() => {
+              handleSubmit();
+            }}
+          />
+        </form>
       </section>
     </main>
   );
 }
 
-export default SignIn
-
+export default SignIn;
